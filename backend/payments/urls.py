5f6432852d2718e2a94ef59b30aadd00a8b48931
zhanerke <29kerimbi@gmail.com>
2026-04-20 23:45:53 +0500

    upd urls payments

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import create_payment_intent, payment_history, stripe_webhook
+from .views import create_payment_intent, payment_history
 
 urlpatterns = [
     path("create-intent/", create_payment_intent, name="create-payment-intent"),
